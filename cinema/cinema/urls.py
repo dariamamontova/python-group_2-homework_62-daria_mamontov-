d117cd57a8116c48e54c76f7173505107e3b2379
Daria <mamontova.dasha@gmail.com>
2019-03-04 18:33:07 +0600

    В API выведена модель Movie. Обновлен файл requirements.txt.

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api_v1.urls', namespace='api_v1'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
