eb75c4f856ac32a279fc9cbc682859a45a02bf13
Daria <mamontova.dasha@gmail.com>
2019-03-07 01:00:28 +0600

    Добавлена модель Booking, зарегистрирована в админке.

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Movie, Category, Hall, Seat, Show, Discount
+from webapp.models import Movie, Category, Hall, Seat, Show, Discount, Ticket, Booking
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'release_date']
@@ -31,10 +31,20 @@ class DiscountAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'discount_price']
     search_fields = ['name', 'id']
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'show', 'seat']
+    search_fields = ['show', 'id']
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['show', 'code', 'get_seats_display']
+    search_fields = ['code', 'id']
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Hall, HallAdmin)
 admin.site.register(Seat, SeatAdmin)
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Discount, DiscountAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Booking, BookingAdmin)
 
--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import random
+import string
+from django.conf import settings
 
 class Movie(models.Model):
     name = models.CharField(max_length=255)
@@ -25,19 +28,22 @@ class Hall(models.Model):
         return self.name
 
 class Seat(models.Model):
-    hall = models.ForeignKey(Hall, on_delete=models.PROTECT, blank=True, verbose_name='Hall', related_name='seats')
-    row = models.IntegerField(null=True, blank=True)
+    hall = models.ForeignKey(Hall, on_delete=models.PROTECT, related_name='seats')
+    row = models.CharField(max_length=10)
     seat = models.CharField(max_length=10)
 
     def __str__(self):
         return self.seat
 
 class Show(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, blank=True, related_name='show_movie')
-    hall = models.ForeignKey(Hall, on_delete=models.PROTECT, blank=True, related_name='show_hall')
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='shows')
+    hall = models.ForeignKey(Hall, on_delete=models.PROTECT, related_name='shows')
     start_date = models.DateTimeField()
     finish_date = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return "%s, %s" % (self.movie, self.hall)
 
 class Discount(models.Model):
     name = models.CharField(max_length=255)
@@ -48,3 +54,42 @@ class Discount(models.Model):
     def __str__(self):
         return self.name
 
+class Ticket(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.PROTECT, related_name='tickets')
+    seat = models.ForeignKey(Seat, on_delete=models.PROTECT, related_name='tickets')
+    discount = models.ForeignKey(Discount, on_delete=models.PROTECT, related_name='tickets', blank=True, null=True)
+    returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Show %s Seat %s" % (self.show, self.seat)
+
+def generate_code():
+    code = ""
+    for i in range(0, settings.BOOKING_CODE_LENGTH):
+        code += random.choice(string.digits)
+    return code
+
+
+BOOKING_STATUS_CHOICES = [
+    ('created', 'Created'),
+    ('sold', 'Sold'),
+    ('canceled', 'Canceled'),
+]
+
+
+class Booking(models.Model):
+    code = models.CharField(max_length=10, unique_for_date='created_at', default=generate_code, editable=False)
+    show = models.ForeignKey(Show, on_delete=models.PROTECT, related_name='booking')
+    seats = models.ManyToManyField(Seat, related_name='booking')
+    status = models.CharField(max_length=20, choices=BOOKING_STATUS_CHOICES, default='created')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s, %s" % (self.show, self.code)
+
+    def get_seats_display(self):
+        seats = ""
+        for seat in self.seats.all():
+            seats += "R%sS%s " % (seat.row, seat.seat)
+            return seats.rstrip()
